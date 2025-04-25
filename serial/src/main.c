8f3287a86e784c68e8c67432e52e9a1df63aee19
Lúcio Mário Barbosa da Silva Filho <luciomariobarbosa10@gmail.com>
2025-04-25 00:03:37 -0300

    feat(serial/main): initial uart communication code (WIP)
    Add first version of the serial/main.c file for UART communication.
    Basic setup, compilation and flashing works but microcontroller
    operation does not work as intended.

---
+++ serial/src/main.c
@@ -0,0 +1,42 @@
+#define F_CPU 16000000UL //External clock frequency used in the Arduino Uno crystal oscillator
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BAUD 9600 //Baud rate in bits per second
+#define MYUBRR ((F_CPU / (16UL * BAUD)) - 1) //Calculates the UBRR value used to determine the 'baud clock' frequency
+
+void uart_init(void)
+{
+    UBRR0H = (uint8_t)(MYUBRR >> 8); //Puts the 8 most significant bits of calculated UBRR
+    UBRR0L = (uint8_t)(MYUBRR); //Puts the 8 least significant bits of calculated UBRR
+    UCSR0A &= ~(1 << U2X0); //Disable asynchrounous double speed mode
+    UCSR0B = (1 << TXEN0); //Enables USART transmitter
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); //Configures the data package bits (8-bit data, 1 stop bit, no parity)
+}
+
+void uart_transmit_byte(const char data)
+{
+    while(!(UCSR0A & (1 << UDRE0))) ; //loops until UDR0 (data buffer) is empty
+    UDR0 = data; //Puts data in data buffer
+}
+
+void uart_transmit_string(const char *data)
+{
+    while(*data)
+        uart_transmit_byte(*data++);
+}
+
+int main(void)
+{
+    uart_init();
+   
+    while(1)
+    {
+        uart_transmit_string("hello\r\n");
+        _delay_ms(3000);
+    }
+    
+    return 0;
+}
+
